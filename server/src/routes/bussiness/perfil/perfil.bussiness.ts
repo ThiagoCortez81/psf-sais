2fb34a9036d7df735eb7bb1a52d18a309db5eeec
Flavio <fla_edu@live.com>
2020-06-04 23:06:54 -0300

    Routes Perfil - Completa

--- server/src/routes/bussiness/perfil/perfil.bussiness.ts
+++ server/src/routes/bussiness/perfil/perfil.bussiness.ts
@@ -1,11 +1,8 @@
 import { conn } from "../../../db_bootstrap";
 
-export async function listPerfil(idPSF?: string,) {
-    // const rows = await conn.query('SELECT * FROM Perfil');
-    // console.log(rows);
-    // return rows.values;
+export async function listPerfil(idPerfil: number,) {
     return new Promise(function (resolve, reject) {
-        conn.query("SELECT * FROM Perfil", function (err, results, fields) {
+        conn.query(`SELECT * FROM Perfil WHERE Id_Perfil = ${idPerfil ? idPerfil : 'Id_Perfil'}`, function (err, results, fields) {
           if (err) return reject(err);
           return resolve(results);
         });
@@ -14,11 +11,45 @@ export async function listPerfil(idPSF?: string,) {
 
 export async function addPerfil(reqBody) {
     return new Promise(function (resolve, reject) {
-        console.log(reqBody);
-        conn.query("INSERT INTO Perfil(desc_perfil_func) VALUES ('Médico')", function (err, results, fields) {
-          if (err) return reject(err);
+
+        let Descricao_Perfil: string = reqBody.desc_perfil_func;
+
+        if(!Descricao_Perfil)  return resolve();
+
+        conn.query(`INSERT INTO Perfil(desc_perfil_func) VALUES ('${Descricao_Perfil}')`, function (err, results, fields) {
+          if (err) return resolve(err);
           return resolve(results);
         });
+
+      });
+}
+
+export async function updatePerfil(reqBody) {
+    return new Promise(function (resolve, reject) {
+
+        let Descricao_Perfil: string = reqBody.desc_perfil_func;
+        let idPerfil: number = parseInt(reqBody.idPerfil);
+
+        if(!idPerfil || !Descricao_Perfil)  return resolve();
+
+        conn.query(`UPDATE Perfil SET desc_perfil_func = '${Descricao_Perfil}' WHERE Id_perfil = ${idPerfil}`, function (err, results, fields) {
+          if (err) return resolve(err);
+          return resolve(results);
+        });
+
+      });
+}
+
+export async function deletePerfil(idPerfil: number,) {
+    return new Promise(function (resolve, reject) {
+
+        if(!idPerfil)  return resolve();
+
+        conn.query(`DELETE FROM Perfil WHERE Id_perfil = ${idPerfil}`, function (err, results, fields) {
+          if (err) return resolve(err);
+          return resolve(results);
+        });
+
       });
 }
 
--- server/src/routes/perfil.router.ts
+++ server/src/routes/perfil.router.ts
@@ -6,18 +6,28 @@ import * as Bussiness from './bussiness/index'
 const router: Router = Router();
 
 router.get('/list',  listPerfil);
-// router.get('/list/:id', PSFSaisServerConfiguration.authenticationMiddleware, listPerfil);
-// router.post('/add', PSFSaisServerConfiguration.authenticationMiddleware, addPerfil);
 router.get('/list/:id', listPerfil);
 router.post('/add', addPerfil);
+router.put('/update', updatePerfil);
+router.delete('/delete/:id', deletePerfil);
 
 async function listPerfil(req: Request, res: Response) {
-    res.send(await Bussiness.listPerfil());
+    let idPerfil: number = parseInt(req.params.id);
+    res.send(await Bussiness.listPerfil(idPerfil));
 }
 
 async function addPerfil(req: Request, res: Response) {
-    res.send(await Bussiness.addPerfil(req));
+    res.send(await Bussiness.addPerfil(req.body));
+}
 
+async function updatePerfil(req: Request, res: Response) {
+    res.send(await Bussiness.updatePerfil(req.body));
+}
+
+async function deletePerfil(req: Request, res: Response) {
+
+    let idPerfil: number = parseInt(req.params.id);
+    res.send(await Bussiness.deletePerfil(idPerfil));
 }
 
 export const PerfilRouter: Router = router;
